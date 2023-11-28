21f2c865a34d1a003aee874c0eb4ec04324b06b7
Alwin K G <alwinkag@gmail.com>
2023-11-28 09:54:04 +0530

    second

---
+++ insertonlyllist.c
@@ -0,0 +1,165 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+struct node   
+{  
+    int data;  
+    struct node *next;   
+};  
+struct node *head;  
+  
+void beginsert ();   
+void lastinsert ();  
+void insertNodeAtMiddle();  
+void display();   
+void main ()  
+{  
+    int choice =0;  
+    while(choice != 9)   
+    {  
+        printf("\nOPERATIONS------\n");  
+        printf("\nChoose one option from the following list.\n");  
+          
+        printf("\n1.Insert in begining\n2.Insert at last\n3.Insert at middle\n4.Display\n5.Exit\n");  
+        printf("\nEnter your choice?\n");         
+        scanf("\n%d",&choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            beginsert();      
+            break;  
+            case 2:  
+            lastinsert();         
+            break;  
+            case 3:  
+            insertNodeAtMiddle();       
+            break;  
+            case 4:  
+            display();        
+            break;  
+            case 5:  
+            exit(0);  
+            break;  
+            default:  
+            printf("Please enter valid choice..");  
+        }  
+    }  
+}  
+void beginsert()  
+{  
+    struct node *ptr;  
+    int item;  
+    ptr = (struct node *) malloc(sizeof(struct node *));  
+    if(ptr == NULL)  
+    {  
+        printf("\nOVERFLOW");  
+    }  
+    else  
+    {  
+        printf("\nEnter value\n");    
+        scanf("%d",&item);    
+        ptr->data = item;  
+        ptr->next = head;  
+        head = ptr;  
+        printf("\nNode inserted");  
+    }  
+      
+}  
+void lastinsert()  
+{  
+    struct node *ptr,*temp;  
+    int item;     
+    ptr = (struct node*)malloc(sizeof(struct node));      
+    if(ptr == NULL)  
+    {  
+        printf("\nOVERFLOW");     
+    }  
+    else  
+    {  
+        printf("\nEnter value?\n");  
+        scanf("%d",&item);  
+        ptr->data = item;  
+        if(head == NULL)  
+        {  
+            ptr -> next = NULL;  
+            head = ptr;  
+            printf("\nNode inserted");  
+        }  
+        else  
+        {  
+            temp = head;  
+            while (temp -> next != NULL)  
+            {  
+                temp = temp -> next;  
+            }  
+            temp->next = ptr;  
+            ptr->next = NULL;  
+            printf("\nNode inserted");  
+          
+        }  
+    }  
+}  
+void insertNodeAtMiddle()
+{
+    int i,data,position;
+    struct node *newNode, *temp;
+
+    newNode = (struct node*)malloc(sizeof(struct node));
+
+    if(newNode == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        newNode->data = data; // Link data part
+        newNode->next = NULL;
+
+        temp = head;
+
+        /* 
+         * Traverse to the n-1 position 
+         */
+        for(i=2; i<=position-1; i++)
+        {
+            temp = temp->next;
+
+            if(temp == NULL)
+                break;
+        }
+
+        if(temp != NULL)
+        {
+            /* Link address part of new node */
+            newNode->next = temp->next; 
+
+            /* Link address part of n-1 node */
+            temp->next = newNode;
+
+            printf("DATA INSERTED SUCCESSFULLY\n");
+        }
+        else
+        {
+            printf("UNABLE TO INSERT DATA AT THE GIVEN POSITION\n");
+        }
+    }
+}
+
+  
+void display()  
+{  
+    struct node *ptr;  
+    ptr = head;   
+    if(ptr == NULL)  
+    {  
+        printf("Nothing to print");  
+    }  
+    else  
+    {  
+        printf("\nprinting values . . . . .\n");   
+        while (ptr!=NULL)  
+        {  
+            printf("\n%d",ptr->data);  
+            ptr = ptr -> next;  
+        }  
+    }  
+}   
